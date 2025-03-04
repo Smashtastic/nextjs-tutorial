b139a79b03671bc7518baa3cbbee0dbe8b4f91c3
Smashtastic <aclough3@agl.com.au>
2025-03-05 10:24:48 +1100

    layout correction

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -39,22 +39,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.customerId &&
-                  state.errors.customerId.map((error: string) => (
-                      <p className="mt-2 text-sm text-red-500" key={error}>
-                        {error}
-                      </p>
-                  ))}
-            </div>
-            <div id="validation-error" aria-live="polite" aria-atomic="true">
-              {state.message &&
-                   (
-                      <p className="mt-2 text-sm text-red-500" key={state.message}>
-                        {state.message}
-                      </p>
-                  )}
-            </div>
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+                state.errors.customerId.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                ))}
           </div>
         </div>
 
@@ -75,14 +67,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-              <div id="amount-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.amount &&
-                    state.errors.amount.map((error: string) => (
-                        <p className="mt-2 text-sm text-red-500" key={error}>
-                          {error}
-                        </p>
-                    ))}
-              </div>
+            </div>
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount &&
+                  state.errors.amount.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                  ))}
             </div>
           </div>
         </div>
@@ -135,6 +127,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                       {error}
                     </p>
                 ))}
+          </div>
+          <div id="validation-error" aria-live="polite" aria-atomic="true">
+            {state.message &&
+                (
+                    <p className="mt-2 text-sm text-red-500" key={state.message}>
+                      {state.message}
+                    </p>
+                )}
           </div>
         </fieldset>
       </div>
